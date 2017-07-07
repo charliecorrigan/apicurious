5a311dd5f0d7edaae0657ed3d39ea4a59325a0a1
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-07 14:49:30 -0600

    Add VCR gem, configure, and wrap logged_in_user_can_see_profile_spec

--- spec/features/logged_in_user_can_see_profile_spec.rb
+++ spec/features/logged_in_user_can_see_profile_spec.rb
@@ -6,19 +6,20 @@ feature "Logged in user visits profile" do
     before(:each) do
       stub_omniauth
     end
-
     it "they click Your profile link" do
-      visit root_path
-      click_on "Login"
-      click_on "Your Profile"
-      user = User.find_by(uid: "12345678")
-      expect(current_path).to eq(user_path(user))
-      expect(page).to have_content("Logout")
-      expect(page).to have_content("charliecorrigan")
-      expect(page).to have_css('img', text: "#{user.avatar_url}")
-      expect(page).to have_content("Stars #{user.starred_repos.count}")
-      expect(page).to have_content("Followers #{user.followers.count}")
-      expect(page).to have_content("Following #{user.followed_users.count}")
+      VCR.use_cassette("github_service.profile_basic_info") do
+        visit root_path
+        click_on "Login"
+        click_on "Your Profile"
+        user = User.find_by(uid: "12345678")
+        expect(current_path).to eq(user_path(user))
+        expect(page).to have_content("Logout")
+        expect(page).to have_content("charliecorrigan")
+        expect(page).to have_css('img', text: "#{user.avatar_url}")
+        expect(page).to have_content("Stars #{user.starred_repos.count}")
+        expect(page).to have_content("Followers #{user.followers.count}")
+        expect(page).to have_content("Following #{user.followed_users.count}")
+      end
     end
   end
 
