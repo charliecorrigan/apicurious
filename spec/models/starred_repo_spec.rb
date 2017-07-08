1ad0cc4f52d485639bebb9caece2e91ac876a1a1
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-07 20:14:11 -0600

    Create starred_repo_spec model test

--- spec/models/starred_repo_spec.rb
+++ spec/models/starred_repo_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe StarredRepo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should belong_to :user }
+
+  it "is a valid starred_repo" do
+    user = User.create(uid: "1234567",
+                    name: "Jimbo",
+                    login: "jimbotronXXX",
+                    token: "superlongstringthatislikeatoken"
+                    )
+
+    raw_starred_repo = {
+      user_id: user.id,
+      full_name: "bobbysue/awesome_repo"
+    }
+
+    starred_repo = StarredRepo.create(raw_starred_repo)
+
+
+    expect(starred_repo.full_name).to eq("bobbysue/awesome_repo")
+    expect(starred_repo.user.uid).to eq("1234567")
+    expect(user.starred_repos.first.full_name).to eq("bobbysue/awesome_repo")
+    expect(user.starred_repos.count).to eq(1)
+  end
 end
