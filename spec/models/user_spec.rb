2a86f0a3bbdce8b6c7dc450562161f081e8a55e0
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-07-07 19:17:56 -0600

    Write valid user test in user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is a valid user" do
+    raw_user = {
+      uid: "1234567",
+      name: "Jimbo",
+      login: "jimbotronXXX",
+      token: "superlongstringthatislikeatoken"
+    }
+
+    user = User.new(raw_user)
+
+    expect(user.uid).to eq("1234567")
+    expect(user.name).to eq("Jimbo")
+    expect(user.login).to eq("jimbotronXXX")
+    expect(user.token).to eq("superlongstringthatislikeatoken")
+  end
 end
